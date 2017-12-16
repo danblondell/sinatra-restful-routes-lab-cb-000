c1528775104a71a8fef1f52c5a6feed7eda36990
Dan Blondell <danblondell@gmail.com>
2017-12-16 05:18:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    @recipe.save
     erb :recipes
   end
 end
