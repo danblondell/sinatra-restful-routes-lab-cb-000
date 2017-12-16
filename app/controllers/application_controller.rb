a94522f76be691ca9085c6f8801335620270f5b5
Dan Blondell <danblondell@gmail.com>
2017-12-16 06:19:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,10 @@ class ApplicationController < Sinatra::Base
     erb :"recipes/edit"
   end
 
+  patch '/recipes/:id/edit' do
+    @recipes = Recipe.find(params[:id])
+  end
+
   delete '/recipes/:id/delete' do
     @recipe = Recipe.find(params[:id])
     @recipe.delete
