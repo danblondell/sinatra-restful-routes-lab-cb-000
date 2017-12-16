df4599d472cd2c21377b283f2f2e046e55be38f6
Dan Blondell <danblondell@gmail.com>
2017-12-16 06:26:32 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,7 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    @recipe.save
 
     erb :"/recipes/show"
   end
